41b5f2f8241a94b0ae591c1cf04145bcdc516b6b
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-12-23 17:44:03 -0600

    Create BOE.nim

---
+++ src/BOE.nim
@@ -0,0 +1,10 @@
+# This file is only for docs! do not import directly
+
+## The BOE docs
+## - `ne<./ne>`_ number/int encoding
+## - `bpe<./bpe>`_ bit precision number/int encoding
+
+
+from BOE/ne import nil
+from BOE/bpe import nil
+
