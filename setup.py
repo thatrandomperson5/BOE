27ce2f2abef49491d91a4284d95e10a555975039
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-12-24 17:18:41 -0600

    Add more details

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 import nimporter
 
+with open("pypi.md", "r") as f:
+    md = f.read()
+
 setup(
     name='intenc',
     version='0.1.0',
     description='Integer encoding',
+    long_description=md,
+    long_description_content_type="text/markdown",
     author='thatrandomperson5',
     url='https://github.com/thatrandomperson5/BOE',
+    license="MIT",
+    classifiers=["Development Status :: 4 - Beta"],
+    keywords=["encoding", "integer", "bit", "byte"],
+    platforms=["linux", "macosx"],   
     packages=['intenc'],
     ext_modules=nimporter.build_nim_extensions(danger=True)
 )
